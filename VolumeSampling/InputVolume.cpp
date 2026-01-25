389bcc4edf68dee173f6f52d0e071af44727cdfe
kaiaiaiii <kaihrig2012@gmail.com>
2026-01-25 18:16:33 +0100

    erweiterung Volume Sampling

--- VolumeSampling/InputVolume.cpp
+++ VolumeSampling/InputVolume.cpp
@@ -1,28 +1,23 @@
-
 #include <iostream>
 using namespace std;
 string MainBodyForm;
 int Resolution;
 int Gridpoints = 0 ;
-int MainMeassurement;
+double Kantenlaenge=7;
+int NumberOfArrayElements = 15;
+double x_0,y_0,z_0;
 int main()
-{   
-
-    cout << "Koerperform (Kugel oder Wuerfel)";
-    cin >> MainBodyForm; 
-    cout << "Hauptabmessung (Radius oder Kantenlaenge)";
-    cin >> MainMeassurement;
-    cout << "Aufloesung (1, 2 oder 3)";
-    cin >> Resolution;
-    
-
-    if (MainBodyForm == Kugel){
-        Gridpoints = 2^(5+2*Resolution);
-        cout << Gridpoints;
-        for(int i =(0 - MainMeassurement); i < (0+MainMeassurement); i++)
-        {
-            
-        } 
+{       
+Gridpoints = 2^(2*Resolution) + 1;
+int arr[NumberOfArrayElements][NumberOfArrayElements][NumberOfArrayElements];
+    for(int i =0; i <= NumberOfArrayElements; i++){
+        for(int j = 0; j <= NumberOfArrayElements; j++){
+            for(int k = 0; k <= NumberOfArrayElements; k++){
+                arr[i][j][k] = Kantenlaenge/(i+1) + Kantenlaenge/(j+1) + Kantenlaenge/(k+1);
+                cout << arr[i][j][k] << " " ;
+            }
+            cout << endl;
+        }
     }
     return 0; 
 }
