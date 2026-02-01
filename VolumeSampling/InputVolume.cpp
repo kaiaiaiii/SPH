922f45a80b36197e85749805bbe2ee87141b5150
kaiaiaiii <kaihrig2012@gmail.com>
2026-02-01 18:49:57 +0100

    volumesampling

--- VolumeSampling/InputVolume.cpp
+++ VolumeSampling/InputVolume.cpp
@@ -4,20 +4,29 @@ string MainBodyForm;
 int Resolution;
 int Gridpoints = 0 ;
 double Kantenlaenge=7;
-int NumberOfArrayElements = 15;
 double x_0,y_0,z_0;
+//vector<double> x_points, y_poinys, z_points = [],[],[];
+int x_len = 5;
+int y_len = 3;
+int z_len = 3;
+double d_xyz = 1; //Abstand zwischen den Partikeln
+int x_Elements = x_len*(1/d_xyz);
+int y_Elements = y_len*(1/d_xyz);
+int z_Elements = z_len*(1/d_xyz);
 int main()
-{       
-Gridpoints = 2^(2*Resolution) + 1;
-int arr[NumberOfArrayElements][NumberOfArrayElements][NumberOfArrayElements];
-    for(int i =0; i <= NumberOfArrayElements; i++){
-        for(int j = 0; j <= NumberOfArrayElements; j++){
-            for(int k = 0; k <= NumberOfArrayElements; k++){
-                arr[i][j][k] = Kantenlaenge/(i+1) + Kantenlaenge/(j+1) + Kantenlaenge/(k+1);
+{ 
+double arr[x_Elements][y_Elements][z_Elements] = {0};
+    for(int i =0; i < x_Elements; i++){
+        for(int j = 0; j < y_Elements; j++){
+            for(int k = 0; k < z_Elements; k++){
+                Gridpoints += 1;
+                arr[i][j][k] += k*d_xyz+j*d_xyz+i*d_xyz;
                 cout << arr[i][j][k] << " " ;
             }
             cout << endl;
         }
-    }
+        cout << endl;
+    } 
+    cout << sizeof(arr)/ sizeof(arr[0][0][0]);
     return 0; 
 }
